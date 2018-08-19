7c21973e603984025e64c0da4bbaf86783e96c83
song <mssongan840136@outlook.com>
2018-08-19 18:17:04 +0900

    add highlight for ruby block

---
+++ elrb-pkg.el
@@ -0,0 +1,5 @@
+(define-package "elrb" "0.0.1"
+  "Emacs Lisp for Ruby Development"
+  '((emacs "24.5")
+    (highlight-indentation "0.5.0"))
+  )
--- elrb.el
+++ elrb.el
@@ -5,6 +5,7 @@
 ;; Author: An Song <songan840136@gmail.com>
 ;; Version: 0.0.1
 ;; Keywords: Ruby, IDE, Tools
+;; Package-Requires: ((emacs "24.5") (highlight-indentation "0.5.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -39,6 +40,13 @@
   :options '(hl-line-mode)
   :group 'elrb)
 
+(defcustom elrb-modules '(elrb-module-highlight-block)
+  "Elrb modules used."
+  :type '(set (const :tag "Display block markers (highlight-indentation)"
+                     elrb-module-highlight-block))
+  :group 'elrb
+  )
+
 (defvar elrb-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-c C-c") 'newline-and-indent)
@@ -46,6 +54,57 @@
 
     map)
   "Key map for the Emacs Lisp Ruby Environment")
+
+;;;;;;;;;;;
+;;; Modules
+
+(defvar elrb-modules-initialized-p nil
+  "Boolean, set to true if modules were run with `global-init'.")
+
+(defun elrb-modules-run (command &rest args)
+  "Run COMMADN with ARGS for all modules in `elrb-modules'."
+  (dolist (module elrb-modules)
+    (apply module command args)))
+
+(defun elrb-modules-global-init ()
+  "Run the global-init method of Elrb modules.
+
+Make sure this only happens once."
+  (when (not elrb-modules-initialized-p)
+    (elrb-modules-run 'global-init)
+    (setq elrb-modules-initialized-p t)))
+
+(defun elrb-modules-global-stop ()
+  "Run the global-stop method of Elrb modules.
+
+Make sure this only happens once per global-init call."
+  (when elrb-modules-initialized-p
+    (elrb-modules-run 'global-stop)
+    (setq elrb-modules-initialized-p nil)))
+
+(defun elrb-modules-buffer-init ()
+  "Run the buffer-init method of Elrb modules.
+
+Make sure global-init is called first."
+  (elrb-modules-global-init)
+  (elrb-modules-run 'buffer-init))
+
+(defun elrb-modules-buffer-stop ()
+  "Run the buffer-stop method of Elrb modules."
+  (elrb-modules-run 'buffer-stop))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Module: Highlight block
+
+(defun elrb-module-highlight-block (command &rest _args)
+  "Module to highlight block in Ruby files."
+  (pcase command
+    (`global-init
+     (require 'highlight-indentation))
+    (`buffer-init
+     (highlight-indentation-mode 1))
+    (`buffer-stop
+     (highlight-indentation-mode -1))))
 
 ;;;;;;;;;;;;;;;;;;;;
 ;;; List Definitions
@@ -63,12 +122,32 @@ And swith to this buffer."
       (switch-to-buffer "*Occur*"))))
 
 ;;;###autoload
+(defun elrb-enable (&optional)
+  "Enable Elrb in all future Ruby buffers."
+  (interactive)
+  (when (< emacs-major-version 24)
+    (error "Elrb requires Emacs 24 or newer."))
+  (elrb-modules-global-init)
+  (add-hook 'ruby-mode-hook 'elrb-mode))
+
+(defun elrb-disable ()
+  "Disable Elrb in all future Ruby buffers."
+  (interactive)
+  (remove-hook 'ruby-mode-hook 'elrb-mode)
+  (elrb-modules-global-stop))
+
+;;;###autoload
 (define-minor-mode elrb-mode
   "Minor mode in Ruby buffers for Emacs Lisp Ruby Development Environment."
   :lighter " Elrb"
   :keymap elrb-mode-map
   (when (not (derived-mode-p 'ruby-mode))
-    (error "Elrb only works with `ruby-mode'")))
+    (error "Elrb only works with `ruby-mode'"))
+  (cond
+   (elrb-mode
+    (elrb-modules-buffer-init))
+   ((not elrb-mode)
+    (elrb-modules-buffer-stop))))
 
 ;;;###autoload
 (defun elrb-version ()
